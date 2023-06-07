24db274706af56b96686d1c059024e76dfbd7e78
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2023-06-07 11:50:20 +0530

    Update 2. Pre Order Traversal.cpp

--- Day 17: Binary Tree/2. Pre Order Traversal.cpp
+++ Day 17: Binary Tree/2. Pre Order Traversal.cpp
@@ -1,3 +1,28 @@
+void PreOrderWithStack(TreeNode* root,vector<int> &ans)
+    {
+        stack< TreeNode * > st;
+        
+        if(root!=NULL)
+        {
+            st.push(root);
+            
+            while(!st.empty())
+            {
+                TreeNode* topv = st.top();
+                st.pop();
+                ans.push_back(topv->val);
+                
+                if(topv->right!=NULL)
+                {
+                    st.push(topv->right);
+                }
+                if(topv->left!=NULL)
+                {
+                    st.push(topv->left);
+                }
+            }
+        }
+    }
 void PreOrderTraversal(TreeNode* root,vector<int> &ans)
     {
         if(root==NULL)  return;
