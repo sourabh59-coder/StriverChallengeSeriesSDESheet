458d106c4059571bcbf044bece31a429e2efce2b
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2023-06-07 11:36:55 +0530

    Update 1. Inorder Traversal.cpp

--- Day 17: Binary Tree/1. Inorder Traversal.cpp
+++ Day 17: Binary Tree/1. Inorder Traversal.cpp
@@ -1,3 +1,29 @@
+void InorderWithStack(TreeNode* r,vector<int> &ans)
+    {
+        stack<TreeNode * > st;
+        
+        while(true)
+        {
+            if(r!=NULL)
+            {
+                st.push(r);
+                r = r->left;
+            }
+            else
+            {
+                if(st.empty())
+                {
+                    break;
+                }
+                
+                r = st.top();
+                st.pop();
+                
+                ans.push_back(r->val);
+                r = r->right;
+            }
+        }
+    }
 void InorderTraversal(TreeNode* root,vector<int> &ans)
     {
         if(root==NULL)  return;
