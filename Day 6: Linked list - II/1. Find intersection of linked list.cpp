fb5d65818f3d776c76c1fe9b1355839ed9acb286
Sourabh Chandel <77907942+sourabh59-coder@users.noreply.github.com>
2023-06-23 10:34:11 +0530

    Update 1. Find intersection of linked list.cpp

--- Day 6: Linked list - II/1. Find intersection of linked list.cpp
+++ Day 6: Linked list - II/1. Find intersection of linked list.cpp
@@ -8,19 +8,28 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
         
-        while(fast!=NULL && fast->next!=NULL)
+        while(ptr1 || ptr2)
         {
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast)
+            if(ptr1==NULL)
             {
-                return true;
+                ptr1 = headB;
             }
+            if(ptr2==NULL)
+            {
+                ptr2 = headA;
+            }
+            if(ptr1==ptr2)
+            {
+                return ptr1;
+            }
+            ptr1 = ptr1->next;
+            ptr2=ptr2->next;
         }
-        return false;
+        
+        return NULL;
     }
 };
